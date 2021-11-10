5b4096305b18eb2c369030ad5a071c24c89b4c2d
egranum <elliot.granum@ways.no>
2021-11-10 11:24:56 +0100

    Connect buttons to each other and the DOM

--- script.js
+++ script.js
@@ -27,10 +27,32 @@ const operate = (x, operator, y) => {
 }
 
 //Loop for finding button values
-let data = document.querySelector('div.buttons').childNodes;
+// let data = document.querySelector('div.buttons').childNodes;
 
-for (let i = 0; i < data.length; i++) {
-    if (data[i].nodeType == 1) {
-        console.log(data[i].childNodes[0].nodeValue);
+// for (let i = 0; i < data.length; i++) {
+//     if (data[i].nodeType == 1) {
+//         console.log(data[i].childNodes[0].nodeValue);
+//     }
+// }
+
+// const numberButtons = Array.from(document.querySelectorAll('.number'))
+// numberButtons.forEach(numButton => numButton.addEventListener('click', function(e) {
+//     console.log(numButton.childNodes[0].nodeValue);
+// }))
+
+const allButtons = Array.from(document.querySelectorAll('.button'));
+
+function connectButtons(e) {
+    //choose the correct button, either by click or keyboard
+    let theCode;
+    if (e.type === 'click') {
+        theCode = e.currentTarget;
+    } else {
+        theCode = document.querySelector(`button[data-key="${e.code}"]`);
     }
+
+    //theCode.childNodes[0].nodeValue  <- the actual number on the button
 }
+
+window.addEventListener('keydown', connectButtons);
+allButtons.forEach(btn => btn.addEventListener('click', connectButtons));
