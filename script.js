508bac6885e24cbc7d658ab16fdb7a7c015cb4d3
egranum <elliot.granum@ways.no>
2021-11-16 13:13:47 +0100

    add pressed buttons to display

--- script.js
+++ script.js
@@ -26,21 +26,16 @@ const operate = (x, operator, y) => {
     return equations[operator];
 }
 
-//Loop for finding button values
-// let data = document.querySelector('div.buttons').childNodes;
-
-// for (let i = 0; i < data.length; i++) {
-//     if (data[i].nodeType == 1) {
-//         console.log(data[i].childNodes[0].nodeValue);
-//     }
-// }
-
-// const numberButtons = Array.from(document.querySelectorAll('.number'))
-// numberButtons.forEach(numButton => numButton.addEventListener('click', function(e) {
-//     console.log(numButton.childNodes[0].nodeValue);
-// }))
+const addToDisplay = (toBeAdded) => {
+    //Add numbers and operators to display
+    const numberContainer = document.querySelector('.number-container');
+    let numberToAdd = document.createElement('span');
+    numberToAdd.textContent = toBeAdded;
+    numberContainer.appendChild(numberToAdd);
+}
 
 const allButtons = Array.from(document.querySelectorAll('.button'));
+
 
 function connectButtons(e) {
     //choose the correct button, either by click or keyboard
@@ -50,9 +45,16 @@ function connectButtons(e) {
     } else {
         theCode = document.querySelector(`button[data-key="${e.code}"]`);
     }
-
-    //theCode.childNodes[0].nodeValue  <- the actual number on the button
+    
+    addToDisplay(theCode.value)
 }
 
-window.addEventListener('keydown', connectButtons);
-allButtons.forEach(btn => btn.addEventListener('click', connectButtons));
+function createVariable(connectButtons) {
+    let showingNumber;
+    showingNumber = connectButtons(e);
+
+    console.log(showingNumber)
+}
+
+window.addEventListener('keydown', connectButtons, createVariable);
+allButtons.forEach(btn => btn.addEventListener('click', connectButtons, createVariable));
