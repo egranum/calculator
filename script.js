b296673d4ddf0a89595e9c5b652141aa472972ac
egranum <elliot.granum@ways.no>
2021-12-08 10:53:46 +0100

    use closure to make shit work

--- script.js
+++ script.js
@@ -26,6 +26,46 @@ const operate = (x, operator, y) => {
     return equations[operator];
 }
 
+// function resetEverything(showingNumber, x, y, operator, numButton) {
+//     showingNumber = "";
+//     x = undefined;
+//     y = undefined;
+//     operator = "";
+//     numButton = undefined;
+//     console.log(showingNumber, x, y, operator, numButton);
+// }
+
+// function doButtonStuff(theCode, showingNumber, x, y, operator, numButton) {
+//     if (theCode.classList.contains("reset")) {
+//         removeDisplayContent();
+//         resetEverything(showingNumber, x, y, operator, numButton);
+//     } else if (theCode.classList.contains("number")) {
+//         numButton = theCode.value
+//         console.log(`this is numbutton inside ${numButton}`)
+//         const createShowing = () => {
+//             console.log(showingNumber += theCode.value);
+//         }
+//         createShowing();
+//         console.log(`this is showing inside ${showingNumber}`)
+//         return createShowing;
+//     } else if (theCode.classList.contains("operator")) {
+//         operator = theCode.value;
+//         console.log(operator);
+//     } 
+// }
+// function doButtonStuff(theCode, showingNumber, x, y, operator, numButton) {
+//     if (theCode.classList.contains("reset")) {
+//         removeDisplayContent();
+//         resetEverything(showingNumber, x, y, operator, numButton);
+//     } else if (theCode.classList.contains("number")) {
+//         numButton = theCode.value;
+//         console.log(numButton);
+//     } else if (theCode.classList.contains("operator")) {
+//         operator = theCode.value;
+//         console.log(operator);
+//     } 
+// }
+
 const addToDisplay = (toBeAdded) => {
     //Add numbers and operators to display
     const numberContainer = document.querySelector('.number-container');
@@ -45,29 +85,56 @@ function removeDisplayContent() {
 const allButtons = Array.from(document.querySelectorAll('.button'));
 
 
-function connectButtons(e) {
-    //choose the correct button, either by click or keyboard
-    let theCode;
-    if (e.type === 'click') {
-        theCode = e.currentTarget;
-    } else {
-        theCode = document.querySelector(`button[data-key="${e.code}"]`);
-    }
-    
-    
-    if (theCode.value == "C") {
-        removeDisplayContent();
-    } else {
+function calculate() {
+    let showingNumber = "";
+    let x;
+    let y;
+    let operator = "";
+    let numButton;
+
+    function connectButtons(e) {
+        //choose the correct button, either by click or keyboard
+        let theCode;
+        if (e.type === 'click') {
+            theCode = e.currentTarget;
+        } else {
+            theCode = document.querySelector(`button[data-key="${e.code}"]`);
+        }
+ 
         addToDisplay(theCode.value)
+        
+        const doButtonStuff = () => {
+            if (theCode.classList.contains("reset")) {
+                removeDisplayContent();
+                const resetEverything = () => {
+                    showingNumber = "";
+                    x = undefined;
+                    y = undefined;
+                    operator = "";
+                    numButton = undefined;
+                    console.log(showingNumber, x, y, operator, numButton);
+                }
+                resetEverything();
+            } else if (theCode.classList.contains("number")) {
+                numButton = theCode.value;
+                const createShowing = () => {
+                    showingNumber += numButton;
+                }
+                createShowing();
+            } else if (theCode.classList.contains("operator")) {
+                operator = theCode.value;
+                console.log(operator);
+            } 
+        }
+        
+        doButtonStuff();
+        console.log(`showing after ${showingNumber}`)
     }
+    
+    window.addEventListener('keydown', connectButtons);
+    // window.addEventListener('keydown', testLog);
+    allButtons.forEach(btn => btn.addEventListener('click', connectButtons));
+    // allButtons.forEach(btn => btn.addEventListener('click', testLog));
 }
 
-function createVariable(connectButtons) {
-    let showingNumber;
-    showingNumber = connectButtons(e);
-
-    console.log(showingNumber)
-}
-
-window.addEventListener('keydown', connectButtons, createVariable);
-allButtons.forEach(btn => btn.addEventListener('click', connectButtons, createVariable));
+calculate()
